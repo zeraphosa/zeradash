e2fcb4651794051ebe61a12185690a4e18a93000
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-07 12:46:59 +0400

    boilerblate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-import { useState } from "react";
-import Sidebar from "./components/Sidebar";
-import Home from "./pages/Home";
-
 export default function App() {
-  const [toggle, setToggle] = useState(true);
-  const handleToggle = () => {
-    setToggle(!toggle);
-  };
-
-  return (
-    <div className="container-fluid bg-secondary min-vh-100">
-      <div className="row">
-        {toggle && (
-          <div className="col-4 col-md-2 bg-white vh-100 position-fixed">
-            <Sidebar />
-          </div>
-        )}
-        {toggle && <div className="col-4 col-md-2"></div>}
-        <div className="col">
-          <Home handleToggle={handleToggle} />
-        </div>
-      </div>
-    </div>
-  );
+  return <div>App</div>;
 }
