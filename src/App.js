5bf6c8adfa43ab5950d30f9f6b41b77569205efc
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-07 19:22:55 +0400

    sidebar add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
+// import { useState } from "react";
+import Sidebar from "./layout/Sidebar";
+// import Home from "./pages/Home";
+
 export default function App() {
-  return <div>App</div>;
+  // const [toggle, setToggle] = useState(true);
+  // const handleToggle = () => {
+  //   setToggle(!toggle);
+  // };
+
+  return (
+    // <div className="container-fluid bg-secondary min-vh-100">
+    //   <div className="row">
+    //     {toggle && (
+    //       <div className="col-4 col-md-2 bg-white vh-100 position-fixed">
+    //         <Sidebar />
+    //       </div>
+    //     )}
+    //     {toggle && <div className="col-4 col-md-2"></div>}
+    //     <div className="col">
+    //       <Home handleToggle={handleToggle} />
+    //     </div>
+    //   </div>
+    // </div>
+    <div>
+      <Sidebar/>
+    </div>
+  );
 }
---
+++ src/layout/Navbar.js
@@ -0,0 +1,30 @@
+export default function Navbar({ handleToggle }) {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-transparent">
+        <i className="navbar-brand bi bi-justify-left fs-4" onClick={handleToggle}></i>
+        <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+          <i className="bi bi-justify"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="collapsibleNavId">
+          <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+            <li className="nav-item dropdown">
+              <a href="/" className="nav-link dropdown-toggle" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Zera
+              </a>
+              <div className="dropdown-menu" aria-labelledby="dropdownId">
+                <a href="/" className="dropdown-item">
+                  Profile
+                </a>
+                <a href="/" className="dropdown-item">
+                  Settings
+                </a>
+                <a href="/" className="dropdown-item">
+                  Logout
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
