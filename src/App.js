dc7052166accee117acb845b44621865add111b3
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-08 12:49:01 +0400

    sidebar mobile done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import { Icon } from "./components/Icons";
 import Sidebar from "./layout/sidebar/Sidebar";
-import Navbar from "./layout/navbar/Navbar";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Ecommerce from "./pages/ecommerce/Ecommerce";
 import Projects from "./pages/projects/Projects";
@@ -8,12 +9,16 @@ import Data from "./pages/data/Data";
 import "./style.css";
 
 export default function App() {
+  const [sideToggle, setSideToggle] = useState(false);
+ 
   return (
     <BrowserRouter>
       <div className="app">
-        <Sidebar />
-        <Navbar />
+        <Sidebar sideToggle={sideToggle} />
         <div className="content">
+          <button className="nav" onClick={() => setSideToggle(!sideToggle)}>
+            <Icon name="menu" size={35} />
+          </button>
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/ecommerce" element={<Ecommerce />} />
--- src/layout/sidebar/Sidebar.js
+++ src/layout/sidebar/Sidebar.js
@@ -1,31 +1,31 @@
 import { Icon } from "../../components/Icons";
 import "./style.css";
-export default function Sidebar() {
+export default function Sidebar({ sideToggle }) {
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${sideToggle && "side-mob"}`}>
       <div className="sidebar-inner">
         <div className="items">
           <div className="brand">
-            <h1 className="brand-logo">Z</h1>
-            <a href="/" className="brand-text">
+            <h1 className={`brand-letter ${!sideToggle && "d-none"}`}>Z</h1>
+            <a href="/" className={sideToggle ? "d-none" : "brand-text"}>
               <h1>ZERA</h1>
             </a>
           </div>
-          <a href="/" className="item-link">
+          <a href="/" className={`item-link active ${sideToggle && "item-link-mob"}`}>
             <Icon name="dashboard" size={20} />
-            <span>Dashboard</span>
+            <span className={`link-text ${sideToggle && "d-none"}`}>Dashboard</span>
           </a>
-          <a href="/" className="item-link">
+          <a href="/" className={`item-link ${sideToggle && "item-link-mob"}`}>
             <Icon name="ecommerce" size={20} />
-            <span>Ecommerce</span>
+            <span className={`link-text ${sideToggle && "d-none"}`}>Ecommerce</span>
           </a>
-          <a href="/" className="item-link">
+          <a href="/" className={`item-link ${sideToggle && "item-link-mob"}`}>
             <Icon name="projects" size={20} />
-            <span>Projects</span>
+            <span className={`link-text ${sideToggle && "d-none"}`}>Projects</span>
           </a>
-          <a href="/" className="item-link">
+          <a href="/" className={`item-link ${sideToggle && "item-link-mob"}`}>
             <Icon name="data" size={20} />
-            <span>Data</span>
+            <span className={`link-text ${sideToggle && "d-none"}`}>Data</span>
           </a>
         </div>
         <div className="shapes">shapes</div>
