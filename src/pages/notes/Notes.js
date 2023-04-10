d4f080a31a1f2e0d9fde38fc6e55d493a59aa8df
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-10 13:38:47 +0400

    notes ui

--- src/pages/notes/Notes.js
+++ src/pages/notes/Notes.js
@@ -1,3 +1,22 @@
+import "./style.css";
+
 export default function Notes() {
-  return <div>Notes</div>;
+  return (
+    <div className="notes">
+      <div className="notes-inner">
+        <div className="note-nav">
+          <div className="search">
+            <input type="text" placeholder="Search" />
+          </div>
+          <div className="add">
+            <button>Add new note</button>
+          </div>
+        </div>
+        <div className="note-content">
+          <div className="note-header">aas</div>
+          <div className="note-text">asdf</div>
+        </div>
+      </div>
+    </div>
+  );
 }
