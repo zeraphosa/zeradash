203e758751bdcb8ed8542973d2e7aedcd3bc0c8d
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-11 15:29:48 +0400

    note ui fix

--- src/pages/notes/Notes.js
+++ src/pages/notes/Notes.js
@@ -3,31 +3,33 @@ import "./style.css";
 
 export default function Notes() {
   return (
-    <div className="notes">
-      <div className="note-nav">
-        <div className="search">
-          <input type="text" placeholder="Search notes" />
-        </div>
-        <div className="note-header">
-          {data.map((item, id) => (
-            <div key={id} className="header-item">
-              <div className="item-text">
-                <p>{item.text}</p>
-                <span>{item.date}</span>
+    <div className="note-container">
+      <div className="notes">
+        <div className="note-nav">
+          <div className="search">
+            <input type="text" placeholder="Search notes" />
+          </div>
+          <div className="note-header">
+            {data.map((item, id) => (
+              <div key={id} className="header-item">
+                <div className="item-text">
+                  <p>{item.text}</p>
+                  <span>{item.date}</span>
+                </div>
+                <div className="close-btn">
+                  <Icon name="close" size={18} />
+                </div>
               </div>
-              <div className="close-btn">
-                <Icon name="close" size={18} />
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="note-content">
-        <div className="add">
-          <button>Add new note</button>
-        </div>
-        <div className="note-text">
-          <textarea placeholder="New Note" wrap="on"></textarea>
+        <div className="note-content">
+          <div className="add">
+            <button>Add new note</button>
+          </div>
+          <div className="note-text">
+            <textarea placeholder="New Note" wrap="on"></textarea>
+          </div>
         </div>
       </div>
     </div>
@@ -39,6 +41,7 @@ const data = [
     text: "header",
     date: "10/04/2023",
   },
+
   {
     text: "header",
     date: "10/04/2023",
