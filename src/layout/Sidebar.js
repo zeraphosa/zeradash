75af531b82fad0bcc429b81d47c4cb7d15763d34
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-13 13:10:47 +0400

    sidebar - dropdown add

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,8 +1,10 @@
 import { Link, NavLink } from "react-router-dom";
 import { Icon } from "../components/Icons";
 import "./style.css";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [dropdown, setDropdown] = useState(false);
   return (
     <div className="sidebar">
       <div className="items">
@@ -11,6 +13,7 @@ export default function Sidebar() {
             <h1>ZERA</h1>
           </Link>
         </div>
+
         <div className="links">
           {pages.map((item, id) => (
             <NavLink key={id} to={item.t} className="navlink">
@@ -18,7 +21,23 @@ export default function Sidebar() {
               <span>{item.n}</span>
             </NavLink>
           ))}
+          <div className="dropdown">
+            <div className="navlink" onClick={() => setDropdown(!dropdown)}>
+              <Icon name="projects" size={20} />
+              <span>Projects</span>
+            </div>
+            <div className="dropdown-menu" style={dropdown ? {display: "flex"} : {display: "none"}}>
+              <span>Project 1</span>
+              <span>Project 2</span>
+              <span>Project 3</span>
+            </div>
+          </div>
         </div>
+
+        <NavLink to="/" className="navlink">
+          <Icon name="ecommerce" size={20} />
+          <span>Deneme</span>
+        </NavLink>
       </div>
       <div className="info">
         <span>Baku, 16C</span>
@@ -31,6 +50,5 @@ export default function Sidebar() {
 const pages = [
   { n: "Dashboard", t: "/", i: "dashboard" },
   { n: "Ecommerce", t: "/ecommerce", i: "ecommerce" },
-  { n: "Projects", t: "/projects", i: "projects" },
   { n: "Notes", t: "/notes", i: "notes" },
 ];
