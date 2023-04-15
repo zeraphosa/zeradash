0f105f94bf0e9d66b3623c80ac6e075f4e20ab91
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-15 12:47:08 +0400

    notes functionality

--- src/pages/notes/Notes.js
+++ src/pages/notes/Notes.js
@@ -1,35 +1,78 @@
 import "./style.css";
 import useTheme from "../../hooks/useTheme";
 import { Icon } from "../../components/Icons";
+import { useState } from "react";
 
 export default function Notes() {
   const [theme] = useTheme();
+  const [active, setActive] = useState(null);
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState("");
+
+  function handleAdd() {
+    setNotes([...notes, { text: note, date: "10/04/2023" }]);
+    setNote("");
+  }
+  function handleShow(item) {
+    setActive(item);
+    setNote(item.text);
+  }
+  function handleDelete(e) {
+    setNotes(notes.filter((item) => item !== e));
+  }
 
   return (
     <div className={`notes ${theme}`}>
-      <div className={`titles ${theme}`}>
+      <div className="titles">
         <div className="title-inner">
-          {data.map((item, id) => (
-            <div key={id} className="title-item">
-              <div>
-                <p>{item.slice(0,30)}</p>
-                <span>15.04.2023</span>
+          {notes.map((item, id) => (
+            <div key={id} className={`title-item ${active === item && "item-active"}`} onClick={() => handleShow(item)}>
+              <div className="title-text">
+                <p>{item.text.slice(0, 30)}</p>
+                <span>{item.date}</span>
               </div>
-              <Icon name="close" size={20} />
+              <div onClick={() => handleDelete(item)}>
+                <Icon name="close" size={20} />
+              </div>
             </div>
           ))}
         </div>
       </div>
       <div className="content">
         <div className="add-btn">
-          <button>Add new note</button>
+          <button onClick={handleAdd}>Add new note</button>
         </div>
         <div className="content-text">
-          <textarea placeholder="New Note" wrap="on" autoFocus="true"></textarea>
+          <textarea placeholder="New Note" wrap="on" autoFocus="true" onChange={(e) => setNote(e.target.value)} value={note}></textarea>
         </div>
       </div>
     </div>
   );
 }
 
-const data = ["Notes headerasdfasdasdfasdfsadfasdfasdfasfdasfasfd", "Notes header", "Notes header", "Notes header", "Notes header", "Notes header",];
+// const data = [
+//   {
+//     h: "notes header",
+//     d: "12.01.2020",
+//   },
+//   {
+//     h: "no312tes header",
+//     d: "13.01.2020",
+//   },
+//   {
+//     h: "notesasdfasd header",
+//     d: "15.12.2020",
+//   },
+//   {
+//     h: "notes header",
+//     d: "21.05.2023",
+//   },
+//   {
+//     h: "nosdafasdfdasffdsaasdffdsafsdafdstes header",
+//     d: "12.01.2020",
+//   },
+//   {
+//     h: "notes header",
+//     d: "12.01.2020",
+//   },
+// ];
