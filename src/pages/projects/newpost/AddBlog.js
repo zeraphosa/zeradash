bb0bf5382b71a9dc8a1c78268f5694c978218462
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-30 11:03:44 +0400

    new quill lib add

--- src/pages/projects/newpost/AddBlog.js
+++ src/pages/projects/newpost/AddBlog.js
@@ -1,20 +1,20 @@
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "./style.scss";
+import { useQuill } from "react-quilljs";
+import "quill/dist/quill.snow.css";
 
 export default function AddBlog() {
+  const { quill, quillRef } = useQuill();
+
   return (
     <div className="post">
+      <div className="save">Save</div>
       <div className="header-container">
-        <div className="post-input">
-          title, comments, tags
-        </div>
+        <div className="post-input">title, comments, tags</div>
         <div className="post-image">image</div>
       </div>
       <div className="article">
-        <Editor toolbarClassName="toolbarClassName" wrapperClassName="wrapperClassName" editorClassName="editorClassName" />
+        <div ref={quillRef}></div>
       </div>
-      <div className="save">Save</div>
     </div>
   );
 }
