a3db939cc404410cc6c29fba5381428d570ebd28
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-30 11:54:01 +0400

    add blog post style

--- src/pages/projects/newpost/AddBlog.js
+++ src/pages/projects/newpost/AddBlog.js
@@ -1,20 +1,52 @@
 import "./style.scss";
 import { useQuill } from "react-quilljs";
+import { Icon } from "../../../components/Icons";
+import useTheme from "../../../hooks/useTheme";
 import "quill/dist/quill.snow.css";
 
 export default function AddBlog() {
-  const { quill, quillRef } = useQuill();
+  const { quillRef } = useQuill();
+  const [theme] = useTheme();
 
   return (
     <div className="post">
-      <div className="save">Save</div>
       <div className="header-container">
-        <div className="post-input">title, comments, tags</div>
-        <div className="post-image">image</div>
+        <div className={`inputs ${theme}`}>
+          <label>
+            Title:
+            <input type="text" placeholder="type title for new post" />
+          </label>
+          <div>
+            <input type="text" placeholder="add new tag" />
+            <button>Add</button>
+          </div>
+          <label>
+            Add comment section
+            <input type="checkbox" />
+          </label>
+        </div>
+        <div className={`info ${theme}`}>
+          <div>Comments</div>
+          <div>Views</div>
+          <div>Likes</div>
+        </div>
       </div>
+
       <div className="article">
         <div ref={quillRef}></div>
+      </div>
+      <div className="save">
+        <button>Save</button>
       </div>
     </div>
   );
 }
+
+// <div className="tags">
+// <button className={`${theme}`}>
+//   technology <Icon name="close" size={15} />
+// </button>
+// <button className={`${theme}`}>
+//   space <Icon name="close" size={15} />
+// </button>
+// </div>
