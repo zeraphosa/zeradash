1692dcd55aa3ff1657108fa4f970c21238d71086
Zaur Sharifov <zaursharifov@outlook.com>
2023-05-07 13:35:52 +0400

    signin style

--- src/pages/login/SignIn.js
+++ src/pages/login/SignIn.js
@@ -8,14 +8,29 @@ export default function SignIn({ isLoggedIn, setIsLoggedIn }) {
   useEffect(() => {
     navigate("/signin");
     if (isLoggedIn) navigate("/");
-    else {return};
+    else {
+      return;
+    }
   }, [isLoggedIn, navigate]);
 
   return (
     <div className="login-container">
       <div className="app-info">Infos</div>
-      <div className="signin">Sign In</div>
-      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>Giris et</button>
+      <div className="signin">
+        <div className="top">
+          <div className="title">Login to ZeraDash</div>
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <div className="submit">
+            <div>Forgot your password ?</div>
+            <button className="sign">Sign In</button>
+          </div>
+        </div>
+        <p>OR</p>
+        <div className="bottom">
+          <button className="sign">Sign Up</button>
+        </div>
+      </div>
     </div>
   );
 }
